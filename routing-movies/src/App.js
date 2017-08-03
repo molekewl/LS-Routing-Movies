be572d82d21e9cbbc086bf5ab5ecc318eea7bf75
John <molekewl2017@gmail.com>
2017-08-03 12:03:25 -0700

    Set up pages, imports & navigation

--- routing-movies/src/App.js
+++ routing-movies/src/App.js
@@ -2,20 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Hello, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  <div>
+    <Navigation />
+    <Route exact path='/' component={Home}/>
+    <Route exact path='/test' component={test}/>
+  </div>
 }
 
 export default App;
--- routing-movies/src/index.js
+++ routing-movies/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreMiddleware(reducers)}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+document.getElementById('root'));
 registerServiceWorker();
